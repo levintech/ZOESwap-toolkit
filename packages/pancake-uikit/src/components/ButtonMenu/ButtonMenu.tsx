18ff1174c1605dc9a5f7539c448545f5e6ac94d2
engineer325 <engineerlife325@gmail.com>
2021-11-11 07:03:28 +1000

    remove border of ButtonMenu

--- packages/pancake-uikit/src/components/ButtonMenu/ButtonMenu.tsx
+++ packages/pancake-uikit/src/components/ButtonMenu/ButtonMenu.tsx
@@ -20,7 +20,6 @@ const StyledButtonMenu = styled.div<StyledButtonMenuProps>`
   background-color: ${getBackgroundColor};
   border-radius: 30px;
   display: ${({ fullWidth }) => (fullWidth ? "flex" : "inline-flex")};
-  border: 1px solid ${getBorderColor};
   width: ${({ fullWidth }) => (fullWidth ? "100%" : "auto")};
 
   & > button,
