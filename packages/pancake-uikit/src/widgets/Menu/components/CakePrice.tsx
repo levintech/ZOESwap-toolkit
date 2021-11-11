1d872ac30c45ce4c8e5adcdd786e80296dad00ef
engineer325 <engineerlife325@gmail.com>
2021-11-11 21:29:46 +1000

    update cakeprice icon

--- packages/pancake-uikit/src/widgets/Menu/components/CakePrice.tsx
+++ packages/pancake-uikit/src/widgets/Menu/components/CakePrice.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { PancakeRoundIcon } from "../../../components/Svg";
+import { LogoIcon } from "../../../components/Svg";
 import Text from "../../../components/Text/Text";
 import Skeleton from "../../../components/Skeleton/Skeleton";
 
@@ -27,7 +27,7 @@ const CakePrice: React.FC<Props> = ({ cakePriceUsd }) => {
       href="https://pancakeswap.finance/swap?outputCurrency=0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82"
       target="_blank"
     >
-      <PancakeRoundIcon width="24px" mr="8px" />
+      <LogoIcon width="24px" mr="8px" />
       <Text color="textSubtle" bold>{`$${cakePriceUsd.toFixed(3)}`}</Text>
     </PriceLink>
   ) : (
